5b48078795f9b220baea9885900ff2897f2c773f
Daniel Standage <daniel.standage@gmail.com>
2014-09-25 15:44:08 -0400

    Added final script for manual calculation of example

---
+++ manual-fisher-combined-p.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from scipy import stats
+from math import log
+
+df     = 4 # k=2 probabilities, 2k=4
+pcount = 0.00313
+pcov   = 0.89476
+
+chi2 = -2 * ( log(pcount) + log(pcov) )
+pvalue = stats.chisqprob(chi2, df)
+print "Chi2 value: %.3f" % chi2
+print "Combined Pvalue: %.5f" % pvalue
